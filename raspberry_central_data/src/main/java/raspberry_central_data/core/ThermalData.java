b21a4e184431b74f165dda9ea0d88a52a47d8559
Lucas Bez <61715273+AlexandreBez@users.noreply.github.com>
2024-12-13 16:14:02 -0300

    V1.1.1

--- raspberry_central_data/src/main/java/raspberry_central_data/core/ThermalData.java
+++ raspberry_central_data/src/main/java/raspberry_central_data/core/ThermalData.java
@@ -1,31 +1,28 @@
 package raspberry_central_data.core;
 
-import com.pi4j.context.Context;
-
-import raspberry_central_data.configs.AMG8833Config;
+import com.pi4j.io.i2c.I2C;
 
 public class ThermalData {
 	
 	public ThermalData() {};
 	public double[][] temperatures;
 
-	public double[][] updateTemperatures(AMG8833Config amg8833Config, Context context) {
-        try {
-        	temperatures = amg8833Config.FRAME;
-            byte[] buffer = new byte[64 * 2];
-            amg8833Config.AMG8833I2CConfig(context).readRegister(0x80, buffer, 0, buffer.length);
+	public double[][] updateTemperatures(I2C i2c, double[][] frame) {
+	    try {
+	        temperatures = frame;
+	        byte[] buffer = new byte[64 * 2];
+	        i2c.readRegister(0x80, buffer, 0, buffer.length);
 
-            for (int i = 0; i < 64; i++) {
-                int tempRaw = ((buffer[i * 2 + 1] & 0xFF) << 8) | (buffer[i * 2] & 0xFF);
-                temperatures[i / 8][i % 8] = tempRaw * 0.25;
-            }
-            //monitorCriticalTemperatures();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        return temperatures;
-    }
+	        for (int i = 0; i < 64; i++) {
+	            int tempRaw = ((buffer[i * 2 + 1] & 0xFF) << 8) | (buffer[i * 2] & 0xFF);
+	            temperatures[i / 8][i % 8] = tempRaw * 0.25; // Certifique-se de que este valor está correto
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    return temperatures;
+	}
 	
 	public double getMaxTemperature() {
         double maxTemp = Double.MIN_VALUE;
@@ -37,3 +34,4 @@ public class ThermalData {
         return maxTemp;
     }
 }
+
